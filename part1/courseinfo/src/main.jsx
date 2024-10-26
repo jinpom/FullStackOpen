dde51f10dc224521cb5e9e5c8481255e2bfeec23
Jean Ramilo <jeanlouiseramilo@gmail.com>
2024-10-26 23:28:06 +0800

    Complete part1 exercises 1.3.-1.5. (courseinfo)

--- part1/courseinfo/src/main.jsx
+++ part1/courseinfo/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
