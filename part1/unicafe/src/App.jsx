bf5647e316998ee2b9c2128b26bdbba9bbaa8bb4
Jean Ramilo <jeanlouiseramilo@gmail.com>
2024-10-28 02:50:09 +0800

    Add exercises 1.6-1.11
    Unicafe costumer feedback app

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+const Statistics = (props) => {
+  if (props.feedbackCount === 0) {
+    return <div>No given feedback</div>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.feedbackCount} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={props.positive + ' %'} />
+      </tbody>
+    </table>
+  );
+};
 
-export default App
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [feedbackCount, setFeedbackCount] = useState(0);
+  const [totalScore, SetTotalScore] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    const newFeedBackCount = feedbackCount + 1;
+    const newTotalScore = totalScore + 1;
+    setFeedbackCount(newFeedBackCount);
+    SetTotalScore(newTotalScore);
+    setAverage(newTotalScore / newFeedBackCount);
+    setPositive(((good + 1) / newFeedBackCount) * 100);
+  };
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    const newFeedBackCount = feedbackCount + 1;
+    // No need to compute for new  total scoere
+    // since neutral feedback has 0 point
+    setFeedbackCount(newFeedBackCount);
+    setAverage(totalScore / newFeedBackCount);
+    setPositive((good / newFeedBackCount) * 100);
+  };
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    const newFeedBackCount = feedbackCount + 1;
+    const newTotalScore = totalScore - 1;
+    setFeedbackCount(newFeedBackCount);
+    SetTotalScore(newTotalScore);
+    setAverage(newTotalScore / newFeedBackCount);
+    setPositive((good / newFeedBackCount) * 100);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
+
+      <h1>statistics</h1>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        feedbackCount={feedbackCount}
+        average={average}
+        positive={positive}
+      />
+    </div>
+  );
+};
+
+export default App;
